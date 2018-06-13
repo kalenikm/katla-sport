0934648e171e0a14dca350746d9269bfbfae004b
Mikhail Kalenik <kalenikmisha@gmail.com>
2018-06-13 13:43:17 +0300

    Small refactoring of step1. Delete redundant comments.

--- KatlaSport.DataAccess/ProductCatalogue/ProductCategory.cs
+++ KatlaSport.DataAccess/ProductCatalogue/ProductCategory.cs
@@ -24,7 +24,10 @@ namespace KatlaSport.DataAccess.ProductCatalogue
         /// </summary>
         public string Code { get; set; }
 
-        // TODO STEP 1 - Add "Description" property here.
+        /// <summary>
+        /// Gets or sets a product category description.
+        /// </summary>
+        public string Description { get; set; }
 
         /// <summary>
         /// Gets or sets a value indicating whether a product category is deleted.
@@ -50,11 +53,6 @@ namespace KatlaSport.DataAccess.ProductCatalogue
         /// Gets or sets a timestamp when the product category was updated last time.
         /// </summary>
         public DateTime LastUpdated { get; set; }
-
-        /// <summary>
-        /// Gets or sets a product category description.
-        /// </summary>
-        public string Description { get; set; }
 
         /// <summary>
         /// Gets or sets a collection of products for the product category.
