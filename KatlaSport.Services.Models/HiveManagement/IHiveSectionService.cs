c944735baeddd862816def6e1b30100505db8442
Mikhail Kalenik <kalenikmisha@gmail.com>
2018-06-17 19:50:18 +0300

    Add UpdateHiveSectionRequest model. Add new mapping in HiveManagementMappingProfile. Add UpdateHiveSectionRequestValidator. Add CreateHiveSectionAsync, UpdateHiveSectionAsync and DeleteHiveSectionAsync methods in IHiveSectionService and implement this methods in HiveSectionService. Add AddHiveSection, UpdateHiveSection and DeleteHiveSection methods in HiveSectionsController.

--- KatlaSport.Services.Models/HiveManagement/IHiveSectionService.cs
+++ KatlaSport.Services.Models/HiveManagement/IHiveSectionService.cs
@@ -29,6 +29,28 @@ namespace KatlaSport.Services.HiveManagement
         Task<List<HiveSectionListItem>> GetHiveSectionsAsync(int hiveId);
 
         /// <summary>
+        /// Creates a new hive section.
+        /// </summary>
+        /// <param name="createRequest">A <see cref="UpdateHiveSectionRequest"/>.</param>
+        /// <returns>A <see cref="Task{HiveSection}"/>.</returns>
+        Task<HiveSection> CreateHiveSectionAsync(UpdateHiveSectionRequest createRequest);
+
+        /// <summary>
+        /// Updates an existed hive section.
+        /// </summary>
+        /// <param name="sectionId">A hive section identifier.</param>
+        /// <param name="updateRequest">A <see cref="UpdateHiveSectionRequest"/>.</param>
+        /// <returns>A <see cref="Task{HiveSection}"/>.</returns>
+        Task<HiveSection> UpdateHiveSectionAsync(int sectionId, UpdateHiveSectionRequest updateRequest);
+
+        /// <summary>
+        /// Deletes an existed hive section.
+        /// </summary>
+        /// <param name="sectionId">A hive section identifier.</param>
+        /// <returns>A <see cref="Task"/>.</returns>
+        Task DeleteHiveSectionAsync(int sectionId);
+
+        /// <summary>
         /// Sets deleted status for a hive section.
         /// </summary>
         /// <param name="hiveSectionId">A hive section identifier.</param>
