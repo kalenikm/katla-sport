35e646b2a4f7300904f5c6bcf81aefe6426a8956
Mikhail Kalenik <kalenikmisha@gmail.com>
2018-06-20 00:05:58 +0300

    Add addHiveSection, updateHiveSection and deleteHiveSection in HiveSectionService. Add fields in HiveSection model. Add HiveSectionForm. Small fix of previous step.

--- KatlaSportNg/src/app/hive-management/forms/hive-section-form.component.ts
+++ KatlaSportNg/src/app/hive-management/forms/hive-section-form.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HiveSectionService } from '../services/hive-section.service';
+import { HiveService } from '../services/hive.service';
+import { HiveSection } from '../models/hive-section';
+import { HiveListItem } from '../models/hive-list-item';
 
 @Component({
   selector: 'app-hive-section-form',
@@ -7,8 +12,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HiveSectionFormComponent implements OnInit {
 
-  constructor() { }
+  hiveSection = new HiveSection(0, "", "", false, "", 1);
+  existed = false;
+  hiveSectionId: number;
+  _hiveId: number;
+  hives: HiveListItem[];
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private hiveSectionService: HiveSectionService,
+    private hiveService: HiveService
+  ) { }
 
   ngOnInit() {
+    this.hiveService.getHives().subscribe(h => this.hives = h);
+
+    this.route.params.subscribe(p => {
+      if (p['hiveId'] !== undefined) {
+        this._hiveId = parseInt(p['hiveId']);
+        this.hiveService.getHive(this._hiveId).subscribe(h => this.hiveSection.hiveId = this._hiveId);
+        this.existed = false;
+        return;
+      }});
+
+    this.route.params.subscribe(p => {
+      if (p['id'] !== undefined) {
+      this.hiveSectionId = p['id'];
+      this.hiveSectionService.getHiveSection(this.hiveSectionId).subscribe(h => {this.hiveSection = h; this._hiveId = h.hiveId;});
+      this._hiveId = this.hiveSection.hiveId;
+      this.existed = true;
+      return;
+    }});
+  }
+
+  navigateToSections() {
+    this.router.navigate([`/hive/${this._hiveId}/sections`]);
+  }
+
+  onCancel() {
+    this.navigateToSections();
+  }
+  
+  onSubmit() {
+    this._hiveId = this.hiveSection.hiveId;
+    if (this.existed) {
+      this.hiveSection.id = this.hiveSectionId;
+      this.hiveSectionService.updateHiveSection(this.hiveSection).subscribe(h => this.navigateToSections());
+    } else {
+      this.hiveSectionService.addHiveSection(this.hiveSection).subscribe(h => this.navigateToSections());
+    }
+  }
+
+  onDelete() {
+    this.hiveSectionService.setHiveSectionStatus(this.hiveSectionId, true).subscribe(h => this.hiveSection.isDeleted = true);
+  }
+
+  onUndelete() {
+    this.hiveSectionService.setHiveSectionStatus(this.hiveSectionId, false).subscribe(h => this.hiveSection.isDeleted = false);
+  }
+
+  onPurge() {
+    if (this.hiveSection.isDeleted) {
+      this.hiveSectionService.deleteHiveSection(this.hiveSectionId).subscribe(h => this.navigateToSections());
+    }
   }
 }
